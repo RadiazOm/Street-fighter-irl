5a4ec6fb2fc3afd3915287ca94d2743b51096ebc
RadiazOm <jeffrey.otterloo@gmail.com>
2024-04-04 16:45:01 +0200

    Created a more diverse dataset (localstorage) and started making seperate pages for the different actions: traindata, train algorithm, test predictions, application.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
+import Main from "./pages/main.jsx";
+import GetData from "./pages/GetData.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>,
+        children: [
+            {
+                path: '/',
+                element: <Main/>
+            },
+            {
+                path: '/train',
+                element: <GetData/>
+            }
+        ]
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router}/>
   </React.StrictMode>,
 )
